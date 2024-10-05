98338c3c98c2385f89c4d43b8525d0882f3b0280
Mohammad Al Rafi <arafi304@gmail.com>
2024-10-05 17:39:58 +0600

    Refactored files and filenames

---
+++ index.py
@@ -0,0 +1,31 @@
+from server import HTTPServer
+from time import sleep
+
+def mainPage(req, res):
+    res.body('<center><h1>Home Page</h1></center>')
+    return res
+
+def testPage(req, res):
+    res.body(f'<h1>Test Page</h1>')
+    print(f"Path: {req.path}\nQuery:{req.query}\nHeaders:{req.header}")
+    return res
+
+def postPage(req, res):
+    print(f"Body: {req.body}")
+    data = {'id': 1, 'count': {'A': 3, 'B': 9}}
+    return res.json(data)
+
+def busyPage(req, res):
+    sleep(5)
+    return res.body("<h1>Now I'm free</h1>")
+
+if __name__ == '__main__':
+    server = HTTPServer(port=8888)
+    try:
+        server.route('GET', '/', mainPage)
+        server.route('GET', '/test', testPage)
+        server.route('POST', '/post', postPage)
+        server.route('GET', '/busy', busyPage)
+        server.start()
+    except KeyboardInterrupt:
+        print(' Exiting...')
